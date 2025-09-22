0d29cb1b12032cb98f38380877ad50b8c2a028d0
Cecilie <cjacob20@student.aau.dk>
2025-09-22 16:18:18 +0200

    Added images for projects and updated text in MyProjects.jsx

--- portfolio/src/components/MyProjects.jsx
+++ portfolio/src/components/MyProjects.jsx
@@ -7,8 +7,10 @@ const MyProjects = () => {
       title: "Aku-Log",
       description: "An acupuncture patient logging site with user authentication, patient data logging and admin dashboard. Built with React and Firebase.",
       technologies: ["JS", "React", "Vite", "HTML/CSS", "Firebase"],
-      demoUrl: "#",
-      githubUrl: "#",
+      demoUrl: "https://aku-log.netlify.app",
+      showDemo: true,
+      githubUrl: "https://github.com/cecilieej/akupunktur-app",
+      showGithub: true,
       imageUrl: "/aku-log.png"
     },
     {
@@ -17,8 +19,10 @@ const MyProjects = () => {
       description: "In progress. A clicker-style game that will stimulate your senses as you earn dopamine points. Built with React and Vite.",
       technologies: ["JS", "React", "Vite", "HTML/CSS", "Netlify"],
       demoUrl: "#",
-      githubUrl: "#",
-      imageUrl: "/api/placeholder/400/250"
+      showDemo: true,
+      githubUrl: "https://github.com/cecilieej/dopamine-clicker",
+      showGithub: true,
+      imageUrl: "/placeholder.png"
     },
     // description: "A collaborative task management application with real-time updates, drag-and-drop functionality, and team collaboration features.",
     {
@@ -27,7 +31,9 @@ const MyProjects = () => {
       description: "Calendar designed for households to optimize energy usage during low-cost hours by displaying real-time electricity prices. Built with Java in Android Studio.",
       technologies: ["Android", "Java", "MinStrøm API", "HTML/CSS"],
       demoUrl: "#",
+      showDemo: false,
       githubUrl: "https://github.com/targot19/P8",
+      showGithub: true,
       imageUrl: "\ecohour.png"
     },
     {
@@ -36,26 +42,32 @@ const MyProjects = () => {
       description: "A tool designed during my 8th semester to streamline data entry processes and reduce manual input errors for an engineering company working with PLM systems in M365. Built with Python.",
       technologies: ["Python", "Pandas", "ScikitLearn", "SQL"],
       demoUrl: "#",
+      showDemo: false,
       githubUrl: "#",
+      showGithub: false,
       imageUrl: "/PLM-solution.png"
     },
     {
       id: 5,
       title: "Movie Website",
-      description: "In progress. A platform for myself and friends to import their Letterboxd data to get an overview of which movies they have in common. Built with C#, .NET, Next.js and React",
+      description: "In progress. A platform for myself and friends to import their Letterboxd data to get an overview of which movies they have in common. To be built with C#, .NET, Next.js and React.",
       technologies: ["C#", ".NET", "Next.js", "React", "Vite", "Tailwind CSS"],
       demoUrl: "#",
+      showDemo: false,
       githubUrl: "#",
-      imageUrl: "/api/placeholder/400/250"
+      showGithub: false,
+      imageUrl: "/placeholder.png"
     },
     {
       id: 6,
       title: "Portfolio Website",
-      description: "The site you're viewing right now! A simple portfolio website built with React and Vite. Featuring some smooth animations and a nice dark mode for your (and my own) viewing pleasure.",
+      description: "The site you're viewing right now! A simple portfolio website built with React and Vite. Working on implementing a nice dark mode for your (and my own) viewing pleasure.",
       technologies: ["JS", "React", "Vite", "HTML/CSS", "Vercel"],
-      demoUrl: "#",
-      githubUrl: "#",
-      imageUrl: "/api/placeholder/400/250"
+      demoUrl: "https://cecilieej.vercel.app",
+      showDemo: true,
+      githubUrl: "https://github.com/cecilieej/cej-portfolio",
+      showGithub: true,
+      imageUrl: "/cecilieej_sceenshot.png"
     }
   ]
 
@@ -91,7 +103,8 @@ const MyProjects = () => {
                   ))}
                 </div>
                 <div className="project-links">
-                  <a 
+                  {project.showDemo && (
+                    <a 
                     href={project.demoUrl} 
                     className="btn btn-primary"
                     target="_blank"
@@ -99,7 +112,10 @@ const MyProjects = () => {
                   >
                     Live Demo
                   </a>
-                  <a 
+                  )}
+                  
+                  {project.showGithub && (
+                    <a 
                     href={project.githubUrl} 
                     className="btn btn-secondary"
                     target="_blank"
@@ -107,6 +123,7 @@ const MyProjects = () => {
                   >
                     GitHub
                   </a>
+                  )}
                 </div>
               </div>
             ))}
