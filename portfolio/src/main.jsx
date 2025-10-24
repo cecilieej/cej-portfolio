53d3ba1af7f3e23d1aaf53bce47de6e8a193e7fc
Cecilie <cjacob20@student.aau.dk>
2025-10-24 15:29:35 +0200

    Minor changes to get the dark mode working for css
    Also set default mode to light mode.

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider  defaultTheme="system" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
   </StrictMode>,
