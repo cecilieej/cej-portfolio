12c587f67c9a6e25f007672c04edb7f09a20e49b
Cecilie <cjacob20@student.aau.dk>
2025-11-19 23:45:34 +0100

    Addition of effect for tech stack scroller animation

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react';
 import Header from './components/Header'
 import About from './components/About'
 import Experience from './components/Experience'
 import MyProjects from './components/MyProjects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { initializeScrollers } from './components/ui/scroller-animation'
 import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 function App() {
+
+  useEffect(() => {
+    initializeScrollers();
+  }, []);
+  
   return (
     <div className="App">
       <Header />
